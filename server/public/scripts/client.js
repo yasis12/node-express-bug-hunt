ef8e041c6db245fdb7ec9f76c62a9650a861ae0b
Rafael Yasis <rafaelyasis@gmail.com>
2023-08-06 13:35:48 -0500

    made changes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,31 +1,33 @@
 console.log('script sourced.');
 
 function getQuotes() {
-    // Axios GET request (http request for information from the server)
-    axios.get('/quotes/').then((response) => {
-        // Code that will run on successful response
-        // from the server.
-        console.log(response);
-        // quotesFromServer will be an Array of quotes
-        let quotesFromServer = response.data;
-        let contentDiv = document.querySelector('#content');
-        contentDiv.innerHTML = '';
-        let i = 0;
-        // FIXED? Loop over array of quotes and append to the content div
-        for(let quote of quotesFromServer) {
-            contentDiv.innerHTML += `
-                <p>
-                    "${quote.text}" -${quote.author}
-                    <button onClick="deleteQuote(${i})">Delete</button>
-                </p>
-            `;
-            i += 1;
-        }
-    }).catch((error) => {
-        console.log(error);
-        alert('Something went wrong.');
-    }); // ALWAYS add .catch
+    axios.get('/quotes/')
+        .then((response) => {
+            let quotesFromServer = response.data;
+            displayQuotes(quotesFromServer);
+        })
+        .catch((error) => {
+            console.log(error);
+            alert('Something went wrong.');
+        });
 }
+
+function displayQuotes(quotesFromServer) {
+    let contentDiv = document.querySelector('#content');
+    contentDiv.innerHTML = '';
+    let i = 0;
+    for (let quote of quotesFromServer) {
+        contentDiv.innerHTML += `
+            <p>
+                "${quote.text}" -${quote.author}
+                <button onClick="deleteQuote(${i})">Delete</button>
+            </p>
+        `;
+        i += 1;
+    }
+}
+
+
 // Call the function
 getQuotes();
 
@@ -60,3 +62,33 @@ function deleteQuote(index) {
         alert('Something went wrong.');
     });
 }
+
+
+// //old get quotes function
+
+// function getQuotes() {
+//     // Axios GET request (http request for information from the server)
+//     axios.get('/quotes/').then((response) => {
+//         // Code that will run on successful response
+//         // from the server.
+//         console.log(response);
+//         // quotesFromServer will be an Array of quotes
+//         let quotesFromServer = response.data;
+//         let contentDiv = document.querySelector('#content');
+//         contentDiv.innerHTML = '';
+//         let i = 0;
+//         // FIXED? Loop over array of quotes and append to the content div
+//         for(let quote of quotesFromServer) {
+//             contentDiv.innerHTML += `
+//                 <p>
+//                     "${quote.text}" -${quote.author}
+//                     <button onClick="deleteQuote(${i})">Delete</button>
+//                 </p>
+//             `;
+//             i += 1;
+//         }
+//     }).catch((error) => {
+//         console.log(error);
+//         alert('Something went wrong.');
+//     }); // ALWAYS add .catch
+// }
